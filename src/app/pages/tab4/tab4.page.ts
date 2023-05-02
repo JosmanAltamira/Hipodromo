2a632fd04f7271512e67536b99b76234ce90a43d
Josman Altamira <78773364+JosmanAltamira@users.noreply.github.com>
2023-05-02 00:14:44 -0600

    vista Json

--- src/app/pages/tab4/tab4.page.ts
+++ src/app/pages/tab4/tab4.page.ts
@@ -1,5 +1,11 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
-
+import { Component, ElementRef, OnInit } from '@angular/core';
+import { LoadingController, ModalController } from '@ionic/angular';
+import { CarrerasService } from '../../services/carreras.service';
+import { Carrera } from '../tab1/carrera.model';
+import { Observable } from 'rxjs';
+import { map, tap } from 'rxjs/operators';
+import { JinetesService } from '../../services/jinetes.service';
+import { Jinete } from '../tab2/jinete.model';
 
 @Component({
   selector: 'app-tab4',
@@ -10,7 +16,30 @@ import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 export class Tab4Page implements OnInit  {
 
+  carreras$: Observable<Carrera[]>;
+  jinetes$: Observable<Jinete[]>;
 
-  constructor() { }
-ngOnInit() {}
+
+  constructor(
+    private carrerasService:CarrerasService,
+    private loadingCtrl: LoadingController,
+    private jinetesService:JinetesService, 
+  ) { }
+
+  async ngOnInit() {
+    const loading = await this.loadingCtrl.create({message:'Loading...', spinner: 'circles',});
+    loading.present();
+
+    this.carreras$ = this.carrerasService.getCarreras().pipe(
+      tap((carreras) => {
+        loading.dismiss();
+        return carreras;
+      })
+    );
+    this.jinetes$ = this.jinetesService.getJinetes().pipe(
+      tap((caballos) => {
+        return caballos;
+      })
+    );
+  }
 }
