a0ba1aaf131807e99553ed83a35fee1e6023e76b
JosmanAltamira <josman_altamira@hotmail.com>
2024-01-06 02:33:52 -0600

    Correcion de guards en app-routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -12,17 +12,17 @@ const routes: Routes = [
  
   {
     path: 'add-caballo',
-    canActivate: [AuthGuard],
+    canActivate: [TabsGuard],
     loadChildren: () => import('./pages/caballos/add-caballo/add-caballo.module').then( m => m.AddCaballoPageModule)
   },
   {
     path: 'add-carrera',
-    canActivate: [AuthGuard],
+    canActivate: [TabsGuard],
     loadChildren: () => import('./pages/carreras/add-carrera/add-carrera.module').then( m => m.AddCarreraPageModule)
   },
   {
     path: 'add-jinete',
-    canActivate: [AuthGuard],
+    canActivate: [TabsGuard],
     loadChildren: () => import('./pages/jinetes/add-jinete/add-jinete.module').then( m => m.AddJinetePageModule)
   },
   {
